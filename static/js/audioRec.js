e27ab561534096651126f413b1415abdbfeb086a
well <707376149@qq.com>
2017-03-13 17:13:04 +0800

    debug

--- static/js/audioRec.js
+++ static/js/audioRec.js
@@ -4,7 +4,6 @@ d3.select("#audioSelect").append("button").attr("id","start-recording");
 d3.select("#audioSelect").append("button").attr("id","stop-recording");
 d3.select("#audioSelect").append("button").attr("id","pause-recording");
 d3.select("#audioSelect").append("button").attr("id","resume-recording");
-d3.select("#audioSelect").append("button").attr("id","upload-recording");
 
 d3.select("#recordingStatus").style({
     "position":"absolute",
@@ -26,7 +25,7 @@ d3.select("#start-recording").style({
     'cursor': 'pointer',
     'border-radius': '5px',
     "background-color":"#3469a4",
-     "left":"60px"
+     "left":"120px"
 }).on("click",function(){
     this.disabled = true;
     navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
@@ -46,15 +45,39 @@ d3.select("#stop-recording").style({
     "height": "70px",
     "border": "solid 1px black",
     'cursor': 'pointer',
+    'border-radius': '5px',
     "background-color":"#3469a4",
-    "left":"180px"
+    "left":"480px"
 }).on("click",function(){
     this.disabled = true;
+    mediaRecorder.ondataavailable = function(blob) {
+    var realData=new Object();
+    realData.audioFile='';
+    realData.textFile=blob;
+    var postData=new Object();
+    postData.data=realData;
+    data2=JSON.stringify(postData);
+    console.log(data2);
+    xhr('http://127.0.0.1:8000/vrDate/', data2, function (fileURL) {
+        window.open(fileURL);
+    });
+
+    function xhr(url, data, callback) {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                callback(location.href + request.responseText);
+            }
+        };
+        request.open('POST', url);
+        request.send(data);
+    }
+};
     mediaRecorder.stop();
     d3.select("#start-recording").disabled=false;
     d3.select("#pause-recording").disabled=true;
     d3.select("#recordingStatus").html("停止");
-}).html("停止").style({
+}).html("上传").style({
     'color': 'white',
     'vertical-align': 'middle',
     'font-size': '20px',
@@ -71,7 +94,7 @@ d3.select("#pause-recording").style({
     'cursor': 'pointer',
     'border-radius': '5px',
     "background-color":"#3469a4",
-    "left":"300px"
+    "left":"240px"
 }).on("click",function(){
     this.disabled = true;
     mediaRecorder.pause();
@@ -94,60 +117,19 @@ d3.select("#resume-recording").style({
     'cursor': 'pointer',
     'border-radius': '5px',
     "background-color":"#3469a4",
-     "left":"420px"
+     "left":"360px"
 }).on("click",function(){
      this.disabled = true;
      mediaRecorder.resume();
-     d3.selct("pause-recording").disabled=false;
+     d3.select("pause-recording").disabled=false;
      d3.select("#recordingStatus").html("录音中");
-}).html("重录").style({
+}).html("继续").style({
      'color': 'white',
      'vertical-align': 'middle',
      'font-size': '20px',
      'text-align': 'center'
 });
-
-
-d3.select("#upload-recording").style({
-    'position':'absolute',
-    "top" : "100px",
-    "width" : "100px",
-    "height": "70px",
-    "border": "solid 1px black",
-    'cursor': 'pointer',
-    'border-radius': '5px',
-    "background-color":"#3469a4",
-    "left":"540px"
-}).on("click",function(){
-    var fileType = 'audio';
-    var fileName = 'ABCDEF.wav';
-
-    var formData = new FormData();
-    formData.append(fileType + '-filename', fileName);
-    formData.append(fileType + '-blob', blob);
-
-    xhr('save.php', formData, function (fileURL) {
-        window.open(fileURL);
-    });
-
-    function xhr(url, data, callback) {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (request.readyState == 4 && request.status == 200) {
-                callback(location.href + request.responseText);
-            }
-        };
-        request.open('POST', url);
-        request.send(data);
-    }
-    d3.select("#recordingStatus").html("停止");
-}).html("上传").style({
-       'color': 'white',
-       'vertical-align': 'middle',
-       'font-size': '20px',
-       'text-align': 'center'
-});
-
+audiosContainer=document.getElementById('recordingStatus');
 var mediaConstraints = {
                 audio: true
             };
@@ -155,13 +137,18 @@ var mediaRecorder;
 //navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
 
 function onMediaSuccess(stream) {
+    var audio = document.createElement('audio');
+    audio = mergeProps(audio, {
+             controls: true,
+             muted: true,
+             src: URL.createObjectURL(stream)
+         });
+    audio.play();
+    audiosContainer.appendChild(audio);
     mediaRecorder = new MediaStreamRecorder(stream);
-    mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
-    mediaRecorder.ondataavailable = function (blob) {
-        // POST/PUT "Blob" using FormData/XHR2
-        var blobURL = URL.createObjectURL(blob);
-        document.write('<a href="' + blobURL + '">' + blobURL + '</a>');
-    };
+    mediaRecorder.stream = stream;
+    mediaRecorder.recorderType = StereoAudioRecorder;
+    mediaRecorder.mimeType = 'audio/wav';
     mediaRecorder.start(3000);
 }
 
