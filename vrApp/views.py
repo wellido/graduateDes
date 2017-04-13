1fa40d5bf972447df9dc0126517523b28375354d
well <707376149@qq.com>
2017-04-13 10:37:15 +0800

    识别返回功能完成

--- vrApp/views.py
+++ vrApp/views.py
@@ -9,7 +9,7 @@ import sys
 import wave
 import commands
 import os
-import subprocess
+import ssh
 from django.views.decorators.csrf import csrf_exempt
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -17,7 +17,6 @@ sys.setdefaultencoding('utf-8')
 # kaldi run.sh path
 cmd = '/home/april/kaldi/egs/thchs30/online_demo/run.sh'
 sudo_password = 'xiaojing527'
-
 conn = sqlite3.connect("/home/april/graduateDes/db.sqlite3",check_same_thread = False)
 cu=conn.cursor();
 
@@ -35,6 +34,15 @@ def insertToDB(conn, cu, audioFile, textFile,audioBinary):
     cu.execute("INSERT INTO vrApp_vrrecord(audioFile, textFile,audioBinary) VALUES (?,?,?)", (audioFile, textFile,audioBinary) )
     conn.commit()
 
+def sshKaldi():
+    myclient = ssh.SSHClient()
+    myclient.set_missing_host_key_policy(ssh.AutoAddPolicy())
+    myclient.connect("202.115.24.67", port=22, username="april", password="xiaojing527")
+    stdin, stdout, stderr = myclient.exec_command("cd /home/april/kaldi/egs/thchs30/online_demo/;bash run.sh")
+    readRrturn = stdout.read()
+    print readRrturn
+    return readRrturn
+
 @csrf_exempt
 def vrRequst(request):
     if request.method == 'GET':
@@ -51,17 +59,13 @@ def vrRequst(request):
         postDict['audioBinary']=req['audioBinary']
         # with open("audio.wav", 'wb') as file:
         #     file.write(postDict['audioBinary'])
-        # p = os.system('echo %s|sudo -S %s' % (sudo_password, cmd))
-        p = subprocess.Popen(['sudo', '-S']+cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-        while p.poll() == None:
-            line = p.stdout.readline()
-            print line
-            shellResult=""
-            shellResult = shellResult + line
+        kaldiResult = sshKaldi();
+        kaldiPost = {'data':kaldiResult}
+        kaldiPostJson = json.dumps(kaldiPost,ensure_ascii=False)
+        print kaldiPostJson
         if postDict:
             insertToDB(conn,cu,postDict['audioFile'],postDict['textFile'],postDict['audioBinary'])
-            return JsonResponse("ok", safe=False, status=status.HTTP_201_CREATED)
+            return JsonResponse(kaldiPostJson, safe=False, status=status.HTTP_201_CREATED)
         return JsonResponse("not ok", safe=False,status=status.HTTP_400_BAD_REQUEST)
 
 
