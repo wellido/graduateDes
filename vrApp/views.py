a746b2943de0408defe86fb2201fe436a37d2402
well <707376149@qq.com>
2017-04-26 09:22:51 +0800

    添加api实现板块

--- vrApp/views.py
+++ vrApp/views.py
@@ -1,3 +1,4 @@
+#encoding=utf-8
 from django.shortcuts import render
 from rest_framework import status
 from vrApp.models import vrRecord
@@ -9,6 +10,9 @@ import sys
 import wave
 import commands
 import os
+import urllib, urllib2, pycurl
+import base64
+import StringIO
 import ssh
 from django.views.decorators.csrf import csrf_exempt
 reload(sys)
@@ -35,7 +39,7 @@ def insertToDB(conn, cu, audioFile, textFile,audioBinary):
     cu.execute("INSERT INTO vrApp_vrrecord(audioFile, textFile,audioBinary) "
                "VALUES (?,?,?)", (audioFile, textFile,audioBinary) )
     conn.commit()
-
+# 删除旧wav
 def rmAudio():
     sshClient = ssh.SSHClient()
     sshClient.set_missing_host_key_policy(ssh.AutoAddPolicy())
@@ -46,7 +50,7 @@ def rmAudio():
         print "delete old audio success."
     else :
         print "delete old audio error."
-
+# 远程操纵kaldi
 def sshKaldi():
     sshClient = ssh.SSHClient()
     sshClient.set_missing_host_key_policy(ssh.AutoAddPolicy())
@@ -56,14 +60,59 @@ def sshKaldi():
     readRrturn = stdout.read()
     print readRrturn
     return readRrturn
-
+# kaldi返回结果处理
 def stringSplit(str):
     str1="ce!"
     str2="%WER"
     index1 = str.index(str1)
     index2 = str.index(str2)
     return str[index1+3:index2]
+# api返回结果处理
+def stringSplit2(str):
+    str1=":[\""
+    str2="，\"]"
+    index1 = str.index(str1)
+    index2 = str.index(str2)
+    return str[(index1+3):index2]
+#获取api操纵权
+def get_token():
+    apiKey = "8s01laGyUBMsAnmMMx2DHVVH"
+    secretKey = "230b13a0116c4cfda432412062999c6c"
+    auth_url = "https://openapi.baidu.com/oauth/2.0/token?" \
+               "grant_type=client_credentials&client_id=" \
+               + apiKey + "&client_secret=" + secretKey;
+    res = urllib2.urlopen(auth_url)
+    json_data = res.read()
+    return json.loads(json_data)['access_token']
+# 打印res
+def dump_res(buf):
+    print buf
+# 调用百度api
+def use_cloud(token):
+    fp = wave.open('/home/april/kaldi/egs/thchs30/online_demo/online-data/audio/audio.wav', 'rb')
+    nf = fp.getnframes()
+    f_len = nf * 2
+    audio_data = fp.readframes(nf)
 
+    cuid = "48:d2:24:50:ad:84"
+    srv_url = 'http://vop.baidu.com/server_api' + '?cuid=' + cuid + '&token=' + token
+    http_header = [
+        'Content-Type: audio/pcm; rate=8000',
+        'Content-Length: %d' % f_len
+    ]
+    apiResult = StringIO.StringIO()
+    crulPost = pycurl.Curl()
+    crulPost.setopt(pycurl.URL, str(srv_url))
+    crulPost.setopt(crulPost.HTTPHEADER, http_header)
+    crulPost.setopt(crulPost.POST, 1)
+    crulPost.setopt(crulPost.CONNECTTIMEOUT, 30)
+    crulPost.setopt(crulPost.TIMEOUT, 30)
+    crulPost.setopt(crulPost.WRITEFUNCTION, apiResult.write)
+    crulPost.setopt(crulPost.POSTFIELDS, audio_data)
+    crulPost.setopt(crulPost.POSTFIELDSIZE, f_len)
+    crulPost.perform()
+    return stringSplit2(apiResult.getvalue())
+# 存放数据
 @csrf_exempt
 def vrRequst(request):
     if request.method == 'GET':
@@ -92,7 +141,10 @@ def vrRequst(request):
         else:
             if postDict:
                 insertToDB(conn,cu,postDict['audioFile'],postDict['textFile'],postDict['audioBinary'])
-                return JsonResponse("ok", safe=False, status=status.HTTP_201_CREATED)
+                token = get_token()
+                apiRes = use_cloud(token)
+                apiPostJson = json.dumps(apiRes,ensure_ascii=False)
+                return JsonResponse(apiPostJson, safe=False, status=status.HTTP_201_CREATED)
             return JsonResponse("not ok", safe=False,status=status.HTTP_400_BAD_REQUEST)
 
 
